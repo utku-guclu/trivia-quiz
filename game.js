ae5cc9a2d13680afa29fd4cda5102a30fa88d9cf
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-08-08 21:26:56 +0300

    ampersand

--- game.js
+++ game.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((loadedQuestions) => {
       questions = loadedQuestions.results.map((loadedQuestion) => {
         const formattedQuestion = {
-          question: loadedQuestion.question.replace(/&quot;/g,'"').replace(/&rsquo;/g,'\'').replace(/&#039;/g,'\'')
+          question: loadedQuestion.question.replace(/&quot;/g,'"').replace(/&rsquo;/g,'\'').replace(/&#039;/g,'\'').replace(/&amp;/g,'&')
         };
 
         const answerChoices = [...loadedQuestion.incorrect_answers];
